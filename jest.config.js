fc15bcd489b3da0e0da807e403a83601efc4d772
v4s1n <vasin.oleksii@gmail.com>
2024-10-27 20:33:31 +0100

    jest - fixed bug

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  silent: true,
+};
---
+++ src/pages/caregory/FilterSheets.test.tsx
@@ -0,0 +1,6 @@
+import { cleanup, fireEvent, render } from "@testing-library/react";
+
+test("just test", () => {
+  const res = true;
+  expect(res).toBeTruthy();
+});
