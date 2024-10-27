4089fc92f8bec65b28e374b8c8f8237ecc719b5d
v4s1n <vasin.oleksii@gmail.com>
2024-10-27 15:32:21 +0100

    foother post data

--- src/components/foother/FootherBanner.tsx
+++ src/components/foother/FootherBanner.tsx
@@ -7,14 +7,50 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Text,
 } from "@chakra-ui/react";
 import ButtonRound from "../common/buttons/ButtonRound";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 interface FootherBannerProps extends BoxProps {
   title: string;
 }
 
 const FootherBanner = ({ title, ...props }: FootherBannerProps) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = async (dataForPost: { email: string }) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+
+        {
+          method: "POST",
+          body: JSON.stringify(dataForPost),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const result = await response.json();
+
+      setIsLoading(false);
+      reset();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <Flex
       {...props}
@@ -33,33 +69,53 @@ const FootherBanner = ({ title, ...props }: FootherBannerProps) => {
       >
         {title}
       </Heading>
+
       <FormControl
         maxW={{ base: "310px", xl: "340px" }}
         mt={{ base: "32px", md: "0px" }}
       >
-        <InputGroup>
-          <InputLeftElement>
-            <EmailIcon color="greyText" />
-          </InputLeftElement>
-          <Input
-            type="email"
-            borderRadius="62px"
-            placeholder="Enter your email address"
-            color="greyText"
-            background="white"
+        <form onSubmit={handleSubmit((data: any) => onSubmit(data))}>
+          <InputGroup>
+            <InputLeftElement>
+              <EmailIcon color="greyText" />
+            </InputLeftElement>
+            <Input
+              type="email"
+              borderRadius="62px"
+              placeholder="Enter your email address"
+              color="greyText"
+              background="white"
+              fontSize={{ base: "sm", xl: "md" }}
+              {...register("email", {
+                required: { value: true, message: "This required" },
+                minLength: {
+                  value: 3,
+                  message: "Min length exceeded",
+                },
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email format",
+                },
+              })}
+            />
+          </InputGroup>
+          {errors.email && errors.email.message && (
+            <Text color="red" position="absolute" top="-25%">
+              👇 {errors.email.message.toString()}
+            </Text>
+          )}
+          <ButtonRound
+            colorBtn="white"
+            w="100%"
+            borderColor="black"
+            mt={{ base: "12px", xl: "14px" }}
             fontSize={{ base: "sm", xl: "md" }}
-          />
-        </InputGroup>
-        <ButtonRound
-          colorBtn="white"
-          w="100%"
-          borderColor="black"
-          mt={{ base: "12px", xl: "14px" }}
-          fontSize={{ base: "sm", xl: "md" }}
-          type="submit"
-        >
-          Subscribe to Newsletter
-        </ButtonRound>
+            type="submit"
+            isLoading={isLoading}
+          >
+            Subscribe to Newsletter
+          </ButtonRound>
+        </form>
       </FormControl>
     </Flex>
   );
